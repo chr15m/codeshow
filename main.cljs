5a7751911504c7cad7e66ae00dba749854e2f693
Chris McCormick <chris@mccormick.cx>
2025-04-19 21:32:09 +0800

    Three dots style.

--- main.cljs
+++ main.cljs
@@ -7,24 +7,24 @@
   "(ns example)\n\n(print (+ 2 3))")
 
 (defn codemirror-editor []
-  (let [editor-instance (r/atom nil)] ; To hold the CodeMirror instance if needed later
-    [:div {:ref (fn [el]
-                  (when el ; el is the DOM node
-                    (let [cm-options #js {:value initial-code
-                                          :mode "clojure"
-                                          :theme "seti"
-                                          :lineNumbers false
-                                          :matchBrackets true
-                                          :autoCloseBrackets true
-                                          :lineWrapping true ; Wrap long lines
-                                          :viewportMargin js/Infinity ; Render all lines for auto height
-                                          }]
-                      ;; Check if CodeMirror is already initialized
-                      (when-not @editor-instance
-                        (let [cm (js/CodeMirror el cm-options)]
-                          ;; Optionally refresh to ensure proper layout
-                          (.refresh cm)
-                          (reset! editor-instance cm))))))}]))
+  (let [editor-instance (r/atom nil)]
+    [:div ;.threedots
+     {:ref (fn [el]
+             (when el ; el is the DOM node
+               (let [cm-options #js {:value initial-code
+                                     :mode "clojure"
+                                     :theme "seti"
+                                     :lineNumbers false
+                                     :matchBrackets true
+                                     :autoCloseBrackets true
+                                     :lineWrapping true ; Wrap long lines
+                                     ; Render all lines for auto height
+                                     :viewportMargin js/Infinity}]
+                 (when-not @editor-instance
+                   (let [cm (js/CodeMirror el cm-options)]
+                     ;; Optionally refresh to ensure proper layout
+                     (.refresh cm)
+                     (reset! editor-instance cm))))))}]))
 
 
 (defn app []
