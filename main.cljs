dfdfd6fda050be8e06d7a061a739496fb6e7c2a2
Chris McCormick <chris@mccormick.cx>
2025-04-19 22:03:45 +0800

    Correctly storing instance.

--- main.cljs
+++ main.cljs
@@ -9,44 +9,47 @@
      :config "{:dots true\n :filename \"example.cljs\"\n :language \"clojure\"}"
      :show-config false}))
 
+(defonce cm-instances
+  (atom {:code nil
+         :config nil}))
+
 (defn config-editor [state]
-  (let [editor-instance (r/atom nil)]
-    [:div.config-editor
-     {:class (when (not (:show-config @state)) "hidden")
-      :ref (fn [el]
-             (when el ; el is the DOM node
-               (let [cm-options #js {:value (:config @state)
-                                     :mode "clojure"
-                                     :theme "seti"
-                                     :lineNumbers false
-                                     :matchBrackets true
-                                     :autoCloseBrackets true
-                                     :lineWrapping true
-                                     :viewportMargin js/Infinity}]
-                 (when-not @editor-instance
-                   (let [cm (js/CodeMirror el cm-options)]
-                     (.refresh cm)
-                     (reset! editor-instance cm))))))}]))
+  [:div.config-editor
+   {:class (when (not (:show-config @state)) "hidden")
+    :ref (fn [el]
+           (when el ; el is the DOM node
+             (let [cm-options #js {:value (:config @state)
+                                   :mode "clojure"
+                                   :theme "seti"
+                                   :lineNumbers false
+                                   :matchBrackets true
+                                   :autoCloseBrackets true
+                                   :lineWrapping true
+                                   :viewportMargin js/Infinity}]
+               (when-not (:config @cm-instances)
+                 (let [cm (js/CodeMirror el cm-options)]
+                   (.refresh cm)
+                   (swap! cm-instances assoc :config cm))))))}])
 
 (defn codemirror-editor [state]
-  (let [editor-instance (r/atom nil)]
-    [:div.threedots
-     {:ref (fn [el]
-             (when el ; el is the DOM node
-               (let [cm-options #js {:value (:code @state)
-                                     :mode "clojure"
-                                     :theme "seti"
-                                     :lineNumbers false
-                                     :matchBrackets true
-                                     :autoCloseBrackets true
-                                     :lineWrapping true ; Wrap long lines
-                                     ; Render all lines for auto height
-                                     :viewportMargin js/Infinity}]
-                 (when-not @editor-instance
-                   (let [cm (js/CodeMirror el cm-options)]
-                     ;; Optionally refresh to ensure proper layout
-                     (.refresh cm)
-                     (reset! editor-instance cm))))))}]))
+
+  [:div.threedots
+   {:ref (fn [el]
+           (when el ; el is the DOM node
+             (let [cm-options #js {:value (:code @state)
+                                   :mode "clojure"
+                                   :theme "seti"
+                                   :lineNumbers false
+                                   :matchBrackets true
+                                   :autoCloseBrackets true
+                                   :lineWrapping true ; Wrap long lines
+                                   ; Render all lines for auto height
+                                   :viewportMargin js/Infinity}]
+               (when-not (:code @cm-instances)
+                 (let [cm (js/CodeMirror el cm-options)]
+                   ;; Optionally refresh to ensure proper layout
+                   (.refresh cm)
+                   (swap! cm-instances assoc :code cm))))))}])
 
 (defn app [state]
   [:div.app-container
