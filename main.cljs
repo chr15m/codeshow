de5292856394d5912b3451e8a9f93ca413b405b5
Chris McCormick <chris@mccormick.cx>
2025-04-21 22:28:00 +0800

    Format table.

--- main.cljs
+++ main.cljs
@@ -24,7 +24,21 @@
    "zenburn"])
 
 (def modes
-  ["apl" "asciiarmor" "asn.1" "asterisk" "brainfuck" "ceylon" "clike" "clojure" "cmake" "cobol" "coffeescript" "commonlisp" "crystal" "css" "cypher" "d" "dart" "diff" "django" "dockerfile" "dtd" "dylan" "ebnf" "ecl" "eiffel" "elixir" "elm" "erlang" "factor" "fcl" "forth" "fortran" "gas" "gherkin" "go" "groovy" "haml" "handlebars" "haskell" "haskell-literate" "haxe" "htmlembedded" "htmlmixed" "http" "idl" "javascript" "jinja2" "jsx" "julia" "livescript" "lua" "markdown" "mathematica" "mbox" "mirc" "mllike" "modelica" "mscgen" "mumps" "nginx" "nsis" "ntriples" "octave" "oz" "pascal" "pegjs" "perl" "php" "pig" "powershell" "properties" "protobuf" "pug" "puppet" "python" "q" "r" "rpm" "rst" "ruby" "rust" "sas" "sass" "scheme" "shell" "sieve" "slim" "smalltalk" "smarty" "solr" "soy" "sparql" "spreadsheet" "sql" "stex" "stylus" "swift" "tcl" "textile" "tiddlywiki" "tiki" "toml" "tornado" "troff" "ttcn" "ttcn-cfg" "turtle" "twig" "vb" "vbscript" "velocity" "verilog" "vhdl" "vue" "wast" "webidl" "xml" "xquery" "yacas" "yaml" "yaml-frontmatter" "z80"])
+  ["apl" "asciiarmor" "asn.1" "asterisk" "brainfuck" "ceylon" "clike" "clojure"
+   "cmake" "cobol" "coffeescript" "commonlisp" "crystal" "css" "cypher" "d"
+   "dart" "diff" "django" "dockerfile" "dtd" "dylan" "ebnf" "ecl" "eiffel"
+   "elixir" "elm" "erlang" "factor" "fcl" "forth" "fortran" "gas" "gherkin" "go"
+   "groovy" "haml" "handlebars" "haskell" "haskell-literate" "haxe"
+   "htmlembedded" "htmlmixed" "http" "idl" "javascript" "jinja2" "jsx" "julia"
+   "livescript" "lua" "markdown" "mathematica" "mbox" "mirc" "mllike" "modelica"
+   "mscgen" "mumps" "nginx" "nsis" "ntriples" "octave" "oz" "pascal" "pegjs"
+   "perl" "php" "pig" "powershell" "properties" "protobuf" "pug" "puppet"
+   "python" "q" "r" "rpm" "rst" "ruby" "rust" "sas" "sass" "scheme" "shell"
+   "sieve" "slim" "smalltalk" "smarty" "solr" "soy" "sparql" "spreadsheet" "sql"
+   "stex" "stylus" "swift" "tcl" "textile" "tiddlywiki" "tiki" "toml" "tornado"
+   "troff" "ttcn" "ttcn-cfg" "turtle" "twig" "vb" "vbscript" "velocity"
+   "verilog" "vhdl" "vue" "wast" "webidl" "xml" "xquery" "yacas" "yaml"
+   "yaml-frontmatter" "z80"])
 
 (def initial-ui
   {:dots true
